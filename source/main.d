43b18ad2a6aec8801923f2197a408640474922f3
Alex Speranza <alex.speranza@studio.unibo.it>
2020-10-29 19:23:18 +0100

    Main: adds module recognition logic
    Adds recognition of apply module.
    Adds function to print the help message with a first iteration of it.

--- source/main.d
+++ source/main.d
@@ -3,6 +3,34 @@ module main;
 import std.stdio;
 import apply;
 
-void main() {
-    apply.apply("./Gitfile");
+void main(string[] args) {
+    args = args[1..$];
+    if (args.length <= 0) {
+        printHelp("Error: No module has been specified.");
+    } else {
+        string mod = args[0];
+        switch (mod) {
+            case "apply":
+                apply.apply("./Gitfile");
+                break;
+            default:
+                printHelp("Error: Module '" ~ mod ~ "' not recognized.");
+                break;
+        }
+    }
+}
+
+void printHelp(string message = "") {
+    if (message.length != 0) {
+        writeln(message);
+    }
+    writeln(
+    `Usage: git-update <module> [path/to/Gitfile] [options]`, "\n",
+    "\n",
+    `Modules:`, "\n",
+    `  - apply => applies the gitfile configuration of repositories`, "\n",
+    "\n",
+    `Options:`, "\n",
+    `  No option are available yet.`
+    );
 }
