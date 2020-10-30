d5fd73d9ac65f4f815b0458273ba1f798bca25d1
Alex Speranza <alex.speranza@studio.unibo.it>
2020-10-30 08:50:40 +0100

    Main: removes the printHelp function and imports the new module.

--- source/main.d
+++ source/main.d
@@ -1,13 +1,13 @@
 module main;
 
-import std.stdio;
+import print_help;
 import apply;
 import arg_parser;
 
 void main(string[] args) {
     args = args[1..$];
     if (args.length <= 0) {
-        printHelp("Error: No module has been specified.");
+        print_help.printHelp("Error: No module has been specified.");
     } else {
         string mod = args[0];
         switch (mod) {
@@ -15,36 +15,8 @@ void main(string[] args) {
                 apply.apply("./Gitfile");
                 break;
             default:
-                printHelp("Error: Module '" ~ mod ~ "' not recognized.");
+                print_help.printHelp("Error: Module '" ~ mod ~ "' not recognized.");
                 break;
         }
     }
-}
-
-/** Prints help message.
-    parameters:
-    - message: message to display before the help message. 
-               If empty nothing is displayed.
-*/
-private void printHelp(string message = "") {
-  if (message.length != 0) {
-      writeln(message);
-  }
-  // TODO: prettify help string
-  string helpString =
-    `Usage: git-update <module> [path/to/Gitfile] [options]` ~ "\n" ~
-    "\n" ~
-    `Modules:` ~ "\n" ~
-    `  - apply => applies the gitfile configuration of repositories` ~ "\n" ~
-    "\n";
-
-  helpString ~=
-    `Options:` ~ "\n"
-  ;
-  foreach (opt; arg_parser.options)
-  {
-    helpString ~= `  ` ~ opt.shortVersion ~ `, ` ~ opt.longVersion ~ `  ` ~ opt.description ~ "\n";
-  }
-  
-  writeln(helpString);
 }
