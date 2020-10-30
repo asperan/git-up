9e17bcb84a834fc3de36134411741b2d81a11fae
Alex Speranza <alex.speranza@studio.unibo.it>
2020-10-30 09:50:46 +0100

    Main: changes calls to print_help module functions; adds debug argument case in switch.

--- source/main.d
+++ source/main.d
@@ -5,18 +5,22 @@ import apply;
 import arg_parser;
 
 void main(string[] args) {
-    args = args[1..$];
-    if (args.length <= 0) {
-        print_help.printHelp("Error: No module has been specified.");
-    } else {
-        string mod = args[0];
-        switch (mod) {
-            case "apply":
-                apply.apply("./Gitfile");
-                break;
-            default:
-                print_help.printHelp("Error: Module '" ~ mod ~ "' not recognized.");
-                break;
+  args = args[1..$];
+  if (args.length <= 0) {
+    print_help.helpMessage("Error: No module has been specified.");
+  } else {
+    string mod = args[0];
+    switch (mod) {
+      case "apply":
+        apply.apply("./Gitfile");
+        break;
+      case "-v":
+      case "--version":
+        print_help.versionMessage();
+        break;
+      default:
+        print_help.helpMessage("Error: Module '" ~ mod ~ "' not recognized.");
+        break;
         }
     }
 }
