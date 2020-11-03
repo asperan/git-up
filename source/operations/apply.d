b592879172fb1204b867a39d892eefe4464f1e7a
Alex Speranza <alex.speranza@studio.unibo.it>
2020-11-03 19:17:13 +0100

    Apply: Changes type of options array.

--- source/operations/apply.d
+++ source/operations/apply.d
@@ -4,12 +4,13 @@ import std.stdio;
 
 import file_parser;
 import repository;
+import file_option;
 
 /**
     Load yaml file and apply the specified configuration.
 */
 void apply(string gitfilePath) {
-  bool[string] options;
+  RuntimeFileOption[] options;
   LocalRepository[] repoInfo;
   loadFile(gitfilePath, options, repoInfo);
   writefln("Searching gitfile: " ~ (gitfilePath == null ? "Gitfile" : gitfilePath ));
