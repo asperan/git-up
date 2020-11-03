057f06f8627fe3241f122706acaad702d408301f
Alex Speranza <alex.speranza@studio.unibo.it>
2020-11-03 18:32:57 +0100

    Repository: changes type to class to implement NamedParsable interface; adds asserts in constructor; adds implementation on name(); fixes field names; fixes opEquals.

--- source/repository.d
+++ source/repository.d
@@ -1,6 +1,7 @@
 module repository;
 
 import std.regex;
+import parsing_utils;
 
 private immutable auto commitRegex = regex(`([0-9a-f]{4,40})`);
 
@@ -14,11 +15,11 @@ enum TreeReferenceType {
 /**
     Local repository object. Represents a local repository based on the cloned remote repository and the local path where it is cloned.
 */
-struct LocalRepository {
+class LocalRepository : NamedParsable {
   private:
     string host;
     string author;
-    string name;
+    string p_name;
 
     string localPath;
 
@@ -35,12 +36,17 @@ struct LocalRepository {
     */
     this( string host, 
           string author, 
-          string name, 
+          string p_name, 
           string localPath, 
           TreeReferenceType treeReferenceType,
           string treeReference,
           string installScriptPath)
     in {
+      assert(host != "null");
+      assert(author != "null");
+      assert(p_name != "null");
+      assert(localPath != "null");
+      assert(treeReference != "null");
       if (treeReferenceType == TreeReferenceType.COMMIT) {
         auto matchResult = treeReference.matchAll(commitRegex); // @suppress(dscanner.suspicious.unmodified)
         assert(matchResult.front.hit == treeReference);
@@ -49,21 +55,37 @@ struct LocalRepository {
     do {
       this.host = host;
       this.author = author;
-      this.name = name;
+      this.p_name = p_name;
       this.localPath = localPath;
       this.refType = treeReferenceType;
       this.treeReference = treeReference;
       this.installScriptPath = installScriptPath;
     }
 
-    size_t toHash() const @safe pure nothrow
-    {
-      return host.hashOf(author.hashOf(name.hashOf(localPath.hashOf())));
+    /** Return the full name of the repository. */
+    string name() const {
+      string fullName = "Repository '";
+      fullName ~= host ~ (host[host.length-1..host.length] == "/" ? "" : "/"); //@suppress(dscanner.suspicious.length_subtraction)
+      fullName ~= author ~ "/";
+      fullName ~= p_name ~ "'";
+      return fullName;
     }
 
-    bool opEquals(LocalRepository)(const LocalRepository other) const
+    override size_t toHash() const @safe pure nothrow
     {
-        return host == other.host && author == other.author && name == other.name && localPath == other.localPath;
+      return host.hashOf(author.hashOf(p_name.hashOf(localPath.hashOf())));
+    }
+
+    override bool opEquals(const Object other) const
+    {
+      if (LocalRepository o = cast(LocalRepository) other) {
+        return host == o.host 
+            && author == o.author 
+            && p_name == o.p_name 
+            && localPath == o.localPath;
+      } else {
+        return false;
+      }
     }
 }
 
