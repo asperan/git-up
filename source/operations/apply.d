7a748a154834ee5fd07895f23a5fa67af1f75512
Alex Speranza <alex.speranza@studio.unibo.it>
2020-11-05 12:45:22 +0100

    Apply: implements execution of actions for repositories

--- source/operations/apply.d
+++ source/operations/apply.d
@@ -1,12 +1,14 @@
 module apply;
 
 import std.stdio;
+import std.conv;
+import std.file;
+import core.stdc.stdlib;
+import std.string : toStringz;
 
 import file_parser;
 import repository;
 import file_option;
-import std.conv;
-import std.file;
 
 /**
     Load yaml file and apply the specified configuration.
@@ -15,13 +17,64 @@ void apply(string gitfilePath) {
   RuntimeFileOption[] options;
   LocalRepository[] repoInfo;
   loadFile(gitfilePath, options, repoInfo);
-  // TODO: check actions to do for repositories
-  // TODO: execute actions
   foreach (LocalRepository key; repoInfo)
   {
-    writeln(computeActionForRepo(key, options).to!string);
+    const RepoAction action = computeActionForRepo(key, options);
+    final switch (action) {
+      case RepoAction.CLONE:
+        if (!exists(key.localPath())) {
+          if (searchBooleanOption("createMissingDirs", options)) {
+            string dirCreateCommand;
+            version(linux) { dirCreateCommand = "mkdir -p"; }
+            version(Windows) { dirCreateCommand = "mkdir"; }
+            system((dirCreateCommand ~ " " ~ key.localPath()).toStringz());
+          } else {
+            printExecutionError("apply", "Local path does not exist and createMissingDirs option not enabled.");
+          }
+        }
+        executeCommandAndCheckError("cd '" ~ key.localPath() ~ "' && git init;", "git init failed.");
+        const string remoteURL = key.host() 
+                                ~ (key.host()[key.host().length-1..key.host().length] == "/" ? "" : "/") //@suppress(dscanner.suspicious.length_subtraction)
+                                ~ key.author() ~ "/" 
+                                ~ key.name();
+        executeCommandAndCheckError("cd '" ~ key.localPath() ~ "' && git remote add origin " ~ remoteURL, 
+                                    "git remote failed to add the origin.");
+      goto case;
+      case RepoAction.UPDATE:
+        assert(exists(key.localPath()), "Local path should exist at this point.");
+        // TODO: maybe redirect output
+        executeCommandAndCheckError("cd '" ~ key.localPath() ~ "' && git fetch --all --tags", 
+                                    "git fetch could not fetch remote commits.");
+        string referenceToMerge;
+        if (key.treeReference() == "latest") {
+          executeCommandAndCheckError("cd '" ~ key.localPath() ~ "' && " 
+                                      ~ (key.refType() == TreeReferenceType.COMMIT ? 
+                                        "git log origin/" ~ key.branch() ~ " --first-parent -n 1 --format=\"%H\" " :
+                                        "git tag --sort=committerdate | tail -n 1") 
+                                      ~ " > .git-updater", "Selected last reference could not be retrieved.");
+          referenceToMerge = read(key.localPath() ~ ".git-updater").to!string;
+        } else {
+          referenceToMerge = key.treeReference();
+        }
+        // TODO: maybe redirect output
+        executeCommandAndCheckError("cd '" ~ key.localPath() ~ "' && git merge " ~ referenceToMerge, 
+                                    "git failed to merge.");
+        goto case;
+      case RepoAction.INSTALL:
+        if (key.installScriptPath() != "") {
+          if (!exists(key.installScriptPath())) {
+            printExecutionError("apply", "Install script not found @ " ~ key.installScriptPath());
+          } else {
+            if(system((key.installScriptPath()).toStringz())) {
+              printExecutionError("apply", "Install script has execution errors.");
+            }
+          }
+        }
+        goto case;
+      case RepoAction.NOTHING:
+        break;
+    }
   }
-  writefln("Searching gitfile: " ~ (gitfilePath == null ? "Gitfile" : gitfilePath ));
 }
 
 /** Action to execute on a repository declaration. 
@@ -40,7 +93,6 @@ private enum RepoAction {
 private RepoAction computeActionForRepo(in LocalRepository repoInfo, in RuntimeFileOption[] options) {
   import std.file : exists;
   import core.stdc.stdlib : system;
-  import std.string : toStringz;
 
   string nullDevice;
   version(linux) { nullDevice = "/dev/null"; }
@@ -67,6 +119,18 @@ private RepoAction computeActionForRepo(in LocalRepository repoInfo, in RuntimeF
 
 private void doActionForRepo(in string action, in string repoInfo) {
 
+}
+
+private void printExecutionError(in string operation, in string errorMessage) {
+  stderr.writeln("Execution error for " ~ operation ~ ": " ~ errorMessage);
+  exit(1);
+  assert(0);
+}
+
+private void executeCommandAndCheckError(in string command, in string errorMessage) {
+  if(system(command.toStringz())) {
+    printExecutionError("apply", errorMessage);
+  }
 }
 
 /** Return the value of a runtime option if present. The default value otherwise. */
