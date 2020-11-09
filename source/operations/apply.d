c3b84655d078bc678592d9dc2f9041611f6dc61e
Alex Speranza <alex.speranza@studio.unibo.it>
2020-11-09 20:18:59 +0100

    Apply: adds printVerbose calls and fixes the last writeln of apply function

--- source/operations/apply.d
+++ source/operations/apply.d
@@ -9,21 +9,27 @@ import std.string : toStringz;
 import file_parser;
 import repository;
 import file_option;
+import print_help : printVerbose;
 
 /**
     Load yaml file and apply the specified configuration.
 */
 void apply(string gitfilePath) {
+  printVerbose("Applying configuration in file '" ~ gitfilePath ~ "'...");
   RuntimeFileOption[] options;
   LocalRepository[] repoInfo;
   loadFile(gitfilePath, options, repoInfo);
   foreach (LocalRepository key; repoInfo)
   {
+    printVerbose("Computing action repository '" ~ key.fullName() ~ "'...");
     const RepoAction action = computeActionForRepo(key, options);
+    printVerbose("Action: " ~ action.to!string);
     final switch (action) {
       case RepoAction.CLONE:
+        printVerbose("Checking existence of localPath...");
         if (!exists(key.localPath())) {
           if (searchBooleanOption("createMissingDirs", options)) {
+            printVerbose("Creating new directory at '" ~ key.localPath() ~ "'...");
             string dirCreateCommand;
             version(linux) { dirCreateCommand = "mkdir -p"; }
             version(Windows) { dirCreateCommand = "mkdir"; }
@@ -32,21 +38,26 @@ void apply(string gitfilePath) {
             printExecutionError("apply", "Local path does not exist and createMissingDirs option not enabled.");
           }
         }
+        printVerbose("Initializing directory...");
         executeCommandAndCheckError("cd '" ~ key.localPath() ~ "' && git init", "git init failed.");
         const string remoteURL = key.host() 
                                 ~ (key.host()[key.host().length-1..key.host().length] == "/" ? "" : "/") //@suppress(dscanner.suspicious.length_subtraction)
                                 ~ key.author() ~ "/" 
                                 ~ key.name();
+        printVerbose("Adding remote '" ~ remoteURL ~ "' named 'origin'");
         executeCommandAndCheckError("cd '" ~ key.localPath() ~ "' && git remote add origin " ~ remoteURL, 
                                     "git remote failed to add the origin.");
+        printVerbose("Initializing completed.");  
       goto case;
       case RepoAction.UPDATE:
         assert(exists(key.localPath()), "Local path should exist at this point.");
         // TODO: maybe redirect output
+        printVerbose("Fetching commits and tags from all remote branches...");
         executeCommandAndCheckError("cd '" ~ key.localPath() ~ "' && git fetch --all --tags", 
                                     "git fetch could not fetch remote commits.");
         string referenceToMerge;
         if (key.treeReference() == "latest") {
+          printVerbose("Retrieving latest " ~ (key.refType() == TreeReferenceType.COMMIT ? "commit" : "tag") ~ "...");
           executeCommandAndCheckError("cd '" ~ key.localPath() ~ "' && " 
                                       ~ (key.refType() == TreeReferenceType.COMMIT ? 
                                         "git log origin/" ~ key.branch() ~ " --first-parent -n 1 --format=\"%H\" " :
@@ -57,15 +68,18 @@ void apply(string gitfilePath) {
         } else {
           referenceToMerge = key.treeReference();
         }
+        printVerbose("Merging " ~ (key.refType() == TreeReferenceType.COMMIT ? "commit" : "tag") ~ " '" ~ referenceToMerge ~ "'..."); //@suppress(dscanner.style.long_line)
         // TODO: maybe redirect output
         executeCommandAndCheckError("cd '" ~ key.localPath() ~ "' && git merge " ~ referenceToMerge, 
                                     "git failed to merge.");
+        printVerbose("Update complete.");
         goto case;
       case RepoAction.INSTALL:
         if (key.installScriptPath() != "") {
           if (!exists(key.installScriptPath())) {
             printExecutionError("apply", "Install script not found @ " ~ key.installScriptPath());
           } else {
+            printVerbose("Install script is specified and exists. Starting execution...");
             if(system(("(" ~ key.installScriptPath() ~ ")").toStringz())) {
               printExecutionError("apply", "Install script has execution errors.");
             }
@@ -75,7 +89,7 @@ void apply(string gitfilePath) {
       case RepoAction.NOTHING:
         break;
     }
-    writeln(key.fullName() ~ " updated.");
+    writeln("Repository '"~ key.fullName() ~ "' updated successfully.");
   }
 }
 
