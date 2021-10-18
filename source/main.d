4347179523716d13d27e6f6bb458b7b2ed3f00a4
Alex Speranza <alex.speranza@studio.unibo.it>
2021-10-18 13:17:22 +0200

    Refactor main to use the new modules and functions

--- source/main.d
+++ source/main.d
@@ -1,52 +1,20 @@
 module main;
 
-import print_help;
-import apply;
-import arg_parser;
-import core.stdc.stdlib;
+import configuration;
+import utility.logging : printErrorHelpAndExit;
+import utility.parsing : handleRemainingArgs;
 
-int main(string[] args) {
+void main(string[] args) {
+  setExecutableName(args[0]);
   args = args[1..$];
   if (args.length <= 0) {
-    print_help.helpMessage("Error: No operation or lone option has been specified.");
-    return 1;
+    printErrorHelpAndExit("No operation or option specified.");
+  }
+  string[] remainingArgs = parseCliArguments(args);
+  if (remainingArgs.length <= 0) {
+    printErrorHelpAndExit("No operation specified.");
   } else {
-    string operation;
-    string mainArgument;
-    RuntimeOption[] options;
-    arg_parser.parseArguments(args, operation, mainArgument, options);
-    checkLoneOptions(options);
-    RuntimeConfiguration.loadConfiguration(options);
-    switch (operation) {
-      case "apply":
-        apply.apply(mainArgument);
-        break;
-      default:
-        print_help.helpMessage("Error: Operation '" ~ operation ~ "' not recognized.");
-        exit(1);
-    }
-  }
-  return 0;
-}
-
-private void checkLoneOptions(in RuntimeOption[] options) {
-  foreach (option; options)
-  {
-    if (option.option.isLone) {
-      handleLoneOption(option);
-    }
+    handleRemainingArgs(remainingArgs);
   }
 }
 
-private void handleLoneOption(in RuntimeOption option) {
-  final switch(option.option.shortVersion) {
-    case "-h":
-      print_help.helpMessage();
-      exit(0);
-      break;
-    case "-v":
-      print_help.versionMessage();
-      exit(0);
-      break;
-  }
-}
